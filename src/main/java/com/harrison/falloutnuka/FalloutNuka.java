9dc919d553bbcce4097e88fb4d284a4140722f15
Harrison-Flax <hstein@smu.edu>
2026-07-30 17:38:35 -0500

    Finalizing fallout pistol mod as an addon for NukaCraft with correct attributes.

---
+++ src/main/java/com/harrison/falloutnuka/FalloutNuka.java
@@ -0,0 +1,11 @@
+package com.harrison.falloutnuka;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(FalloutNuka.MODID)
+public class FalloutNuka
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "falloutnuka";
+}
