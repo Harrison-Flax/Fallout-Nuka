40c040cf1b349cf6796fb85810676b540c4fed83
Harrison-Flax <hstein@smu.edu>
2026-07-31 00:44:04 -0500

    Fixing pistol properties.

--- src/main/java/com/harrison/falloutnuka/FalloutNuka.java
+++ src/main/java/com/harrison/falloutnuka/FalloutNuka.java
@@ -16,28 +16,4 @@ public class FalloutNuka
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "falloutnuka";
-
-    // Holding the item
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-
-    // Registering the pistol name
-    public static final RegistryObject<Item> FALLOUT_10MM = ITEMS.register("fallout_1_10mm",
-            () -> new Item(new Item.Properties()));
-
-    // Constructor for defining the item in the game
-    public FalloutNuka(FMLJavaModLoadingContext context) {
-        IEventBus modEventBus = context.getModEventBus();
-
-        ITEMS.register(modEventBus);
-
-        // Adds to creative mode menu
-        modEventBus.addListener(this::addCreative);
-    }
-
-    // Pistol will be in combat tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
-            event.accept(FALLOUT_10MM);
-        }
-    }
 }
